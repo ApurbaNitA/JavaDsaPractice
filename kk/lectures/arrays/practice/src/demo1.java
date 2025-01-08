8f4459e98c21e21c13fe2056c3f720de0c1357f7
ApurbaNitA <66513761+ApurbaNitA@users.noreply.github.com>
2025-01-09 02:09:58 +0530

    commit

---
+++ kk/lectures/arrays/practice/src/demo1.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class demo1 {
+    public static void main(String[] args) {
+//        WebDriver driver = new webdriver();
+//        driver.get("http://flipkert.com");
+//        Thread.sleep(1000);
+//        WebElement mailid = driver.findElement(By.id("loginid"));
+//        mailid.sendKeys("apurba@gmail.com");
+//        WebElement password = driver.findElement(By.id("password"));
+//        password.sendKeys("password");
+//        driver.findElement(By.id("sendBtn")).click();
+//        JavascriptExecuter js = (JavascriptExecuter) driver;
+//        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+//
+//
+        String a="143729";
+        int n=2;
+//        min = 1372;
+        System.out.println(findLeastString(a,n));
+
+
+
+
+        int [] arr={1,2,3};
+        int sum=4;
+        ArrayList<int[]>pairs= sum1(arr,sum);
+        for(int[]pair:pairs){
+            System.out.println(Arrays.toString(pair));
+        }
+
+    }
+
+    public static ArrayList<int[]> sum1(int[] arr, int sum) {
+        ArrayList<int[]> list = new ArrayList<>();
+        int n = arr.length;
+        for (int j : arr) {
+            for (int k : arr) {
+                if (j + k == sum) {
+                     list.add(new int[]{j, k});
+                }
+            }
+        }
+        return list;
+    }
+
+    public static String findLeastString(String str, int n) {
+        // Initialize a StringBuilder to construct the result
+        StringBuilder result = new StringBuilder();
+        int len = str.length();
+        int keep = len - n; // Number of characters to keep
+
+        // Iterate through the string
+        for (int i = 0; i < len; i++) {
+            // Remove characters from the result if they're greater than the current character
+            // and we still have characters to remove
+            while (result.length() > 0 && result.charAt(result.length() - 1) > str.charAt(i) && n > 0) {
+                result.deleteCharAt(result.length() - 1);
+                n--;
+            }
+            // Add the current character to the result
+            result.append(str.charAt(i));
+        }
+
+        // Return the first 'keep' characters from the result
+        return result.substring(0, keep);
+    }
+
+
+}
---
+++ kk/lectures/arrays/practice/src/practice.java
@@ -0,0 +1,130 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class practice {
+    public static void main(String[] args) {
+        //String str = "Welcome to Sprinkler";
+        Scanner sc = new Scanner(System.in);
+        String first = sc.nextLine();
+        String second = sc.nextLine();
+        int[]arr={0,0,1,1,1,0,1,0,1,1,0,0,0,0,1};
+        int[]nums={2,3,1,1,1,0,1,5,1,1,5,6,6,6,1};
+        System.out.println(reverse(first, second));
+        System.out.println(nonRepeatingChar("apurba"));
+        System.out.println(countOnes(arr));
+        findDuplicates(nums);
+        System.out.println(Arrays.toString(fibonacci(7)));
+        System.out.println(palindrome("karatarak"));
+        System.out.println(sum(7,8));
+    }
+
+    public static String reverse(String str1, String str2) {
+
+     str1 = str1.replaceAll("\\s","");
+     str2 = str2.replaceAll("\\s","");
+     String rev="";
+
+        for (int i = str1.length()-1; i>=0 ; i--) {
+            rev+=str1.charAt(i);
+        }
+
+        return (rev+str2);
+
+    }
+
+    static boolean anagram(String s1, String s2){
+        char[] str1= s1.toCharArray();
+        char[] str2= s2.toCharArray();
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        return Arrays.equals(str1, str2);
+    }
+
+    static char nonRepeatingChar(String str){
+        int n=str.length();
+        for (int i = 0; i < n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n; j++) {
+                if (i!=j && str.charAt(i)==str.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found == false) {
+                return str.charAt(i);
+            }
+        }
+        return '$';
+    }
+
+    public static int countOnes(int[] arr) {
+
+        int n = arr.length;
+        int count=0;
+        for (int i = 0; i < n; i++) {
+            if(arr[i]==1){
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static void findDuplicates(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+//        for (int num : nums) {
+//            if (!seen.add(num)) {
+//                System.out.println("Duplicate: " + num);
+//            }
+//        }
+        int n=nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i!=j && nums[i]==nums[j]){
+                    System.out.println("duplicate: "+nums[i]);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static int[] fibonacci(int n){
+        int[] arr= new int[n];
+        if (n == 1) {
+            return new int[]{0};
+        } else if (n == 2) {
+            return new int[]{0,1};
+        } else {
+            arr[0]=0;
+            arr[1]=1;
+            for (int i = 2; i < n; i++) {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+        }
+        return arr;
+    }
+
+    public static boolean palindrome(String str){
+        char[] ch = str.toCharArray();
+        int n = ch.length;
+        for (int i = 0; i <n/2 ; i++) {
+            if (ch[i]!=ch[n-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int sum(int a, int b){
+        int sum =0;
+        sum =a+b;
+        return sum;
+    }
+
+}
+
+
+
